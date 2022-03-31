c2e0e4e44dde343ca6e3fb3876c5a13336547c1b
purva-coditas <102578307+purva-coditas@users.noreply.github.com>
2022-03-31 14:57:54 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,54 @@
-import React from "react";
-import Header from "./components/Header";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Header from "./components/Header/Header";
 import Info from "./components/Info";
-import Main from "./components/Main";
-import "./styles/styles.scss";
+import Main from "./components/Main/Main";
+import MUser from "./interface/MUser";
 
 const App = () => {
+  const [users, setUsers] = useState<MUser[] | null>([]);
+  const [searchWord, setSearchWord] = useState("");
+  const [sortBy, setSortBy] = useState("byNameA");
+
+  useEffect(() => {
+    const sortUsers = (sortBy: string) => {
+      let sorted;
+      console.log("In Sort User", sortBy);
+
+      switch (sortBy) {
+        case "byNameA":
+          sorted = [...users].sort((a, b) => a.login.localeCompare(b.login));
+          break;
+        case "byNameD":
+          sorted = [...users].sort((a, b) => b.login.localeCompare(a.login));
+          break;
+        case "byRankA":
+          sorted = [...users].sort((a, b) => a.id - b.id);
+        case "byRankD":
+          sorted = [...users].sort((a, b) => b.id - a.id);
+      }
+
+      console.log("Sorted:", sorted);
+      setUsers(sorted);
+    };
+    axios
+      .get(`https://api.github.com/search/users?q=${searchWord}`)
+      .then((res) => {
+        console.log("fetched one", res.data.items);
+        setUsers(res.data.items);
+
+        // sortUsers(sortBy);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [searchWord, sortBy]);
+
   return (
     <>
       <Info message="GitHub Search App" />
-      <Header />
-      <Main />
+      <Header setSearchWord={setSearchWord} setSortBy={setSortBy} />
+      <Main data={users} />
     </>
   );
 };
